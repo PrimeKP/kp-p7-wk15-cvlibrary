421373f89f24d5e9fbfd6edc4dae9219777835d2
PrimeKP <kaushik.pchase@gmail.com>
2022-12-16 19:54:07 +0000

    kaushik extra project cvlibrary

---
+++ src/test/java/uk/co/library/testsuite/LoginPageTest.java
@@ -0,0 +1,45 @@
+package uk.co.library.testsuite;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import resources.testdata.TestData;
+import uk.co.library.customlisteners.CustomListeners;
+import uk.co.library.pages.HomePage;
+import uk.co.library.pages.ResultPage;
+import uk.co.library.testbase.BaseTest;
+
+/**
+ * Created by Jay Vaghani
+ */
+@Listeners(CustomListeners.class)
+public class LoginPageTest extends BaseTest {
+
+    HomePage homePage;
+    ResultPage resultPage;
+
+    @BeforeMethod
+    public void inIt() {
+        homePage = new HomePage();
+        resultPage = new ResultPage();
+    }
+    @Test(dataProvider = "credentials", dataProviderClass = TestData.class)
+    public void verifyJobSearchResultUsingDifferentDataSet(String enterjobTitle, String enterlocation, String distance,
+                                                           String salaryMin, String salaryMax, String salaryType,
+                                                           String jobType, String result) throws InterruptedException {
+        homePage.acceptIFrameAlert();
+        homePage.enterJobTitle(enterjobTitle);
+        homePage.enterLocation(enterlocation);
+        homePage.selectDistance(distance);
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary(salaryMin);
+        homePage.enterMaxSalary(salaryMax);
+        homePage.selectSalaryType(salaryType);
+        homePage.selectJobType(jobType);
+        homePage.clickOnFindJobsButton();
+        Assert.assertEquals(resultPage.verifyTheResults(result),result,"Result not matching");
+
+    }
+
+}
